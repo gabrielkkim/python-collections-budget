c8e058454c25f40d0d4c8152dd97f2feea296257
gabrielkkim <gabrielkkim@gmail.com>
2020-07-17 10:13:36 -0700

    Update FrequentExpenses.py

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -1,4 +1,5 @@
 from . import Expense
+import collections
 
 expenses = Expense.Expenses()
 expenses.read_expenses('data/spending_data.csv')
@@ -6,3 +7,8 @@ expenses.read_expenses('data/spending_data.csv')
 spending_categories = []
 for expense in expenses.list:
     spending_categories.append(expense.category)
+
+# Collections Counter that count how many purchases were in each category
+# Must import collections
+spending_counter = collections.counter(spending_categories)
+print(spending_counter)

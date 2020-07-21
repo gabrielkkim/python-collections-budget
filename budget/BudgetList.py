823f13cf10a476cc013977a004b9b4f194c4e790
gabrielkkim <gabrielkkim@gmail.com>
2020-07-21 11:07:00 -0700

    Update BudgetList.py

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -8,4 +8,5 @@ class BudgetList():
 
     def append(self, item):
         if (self.sum_expenses+item < self.budget):
-            pass
+            self.expenses.append(item)
+            self.sum_expenses += item

e349e10c191cad13bb393669206c79e63483a80f
gabrielkkim <gabrielkkim@gmail.com>
2020-07-21 17:47:23 -0700

    Update BudgetList.py

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -50,6 +50,7 @@ def main():
     fig, ax = plt.subplots()
     labels = ['Expenses', 'Overages', 'Budget']
     values = [myBudgetList.sum_expenses, myBudgetList.sum_overages, myBudgetList.budget]
+    ax.bar(labels, values, color=['green', 'red', 'love'])
 
 if __name__ == "__main__":
     main()
